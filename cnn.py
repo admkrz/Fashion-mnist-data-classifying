2443e01bfd8f6905e5a6677d1e94182db1f25365
admkrz <adam.krzeminski14@gmail.com>
2020-06-07 10:49:07 +0200

    first commit

---
+++ cnn.py
@@ -0,0 +1,145 @@
+from itertools import product
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D, MaxPool2D, BatchNormalization
+from keras.callbacks import ModelCheckpoint
+from keras.models import load_model
+from sklearn.metrics import confusion_matrix
+
+BATCH_SIZE = 128
+EPOCHS = 20
+
+
+def run_cnn(X_train, X_val, X_test, y_train, y_val, y_test):
+    # Building Sequential model from Keras library
+    model1 = Sequential()
+    model2 = Sequential()
+    model3 = Sequential()
+    # Adding layers to the model
+
+    model1.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', input_shape=(28, 28, 1)))
+    model1.add(MaxPooling2D((2, 2)))
+    model1.add(Flatten())
+    model1.add(Dense(100, activation='relu', kernel_initializer='he_uniform'))
+    model1.add(Dense(10, activation='softmax'))
+
+    model2.add(Conv2D(32, (5, 5), padding="same", input_shape=[28, 28, 1]))
+    model2.add(MaxPool2D((2, 2)))
+    model2.add(Conv2D(64, (5, 5), padding="same"))
+    model2.add(MaxPool2D((2, 2)))
+    model2.add(Flatten())
+    model2.add(Dense(1024, activation='relu'))
+    model2.add(Dropout(0.5))
+    model2.add(Dense(10, activation='softmax'))
+
+    model3.add(Conv2D(32, kernel_size=3, activation='relu', input_shape=(28, 28, 1), padding='same'))
+    model3.add(BatchNormalization())
+    model3.add(Dropout(0.2))
+    model3.add(Conv2D(32, kernel_size=3, activation='relu', padding='same'))
+    model3.add(Dropout(0.2))
+    model3.add(Conv2D(24, kernel_size=3, activation='relu', padding='same'))
+    model3.add(Dropout(0.2))
+    model3.add(Conv2D(64, kernel_size=3, activation='relu', padding='same'))
+    model3.add(MaxPooling2D(pool_size=(2, 2)))
+    model3.add(Dropout(0.2))
+    model3.add(Flatten())
+    model3.add(Dense(128, activation='relu'))
+    model3.add(Dropout(0.3))
+    model3.add(Dense(10, activation='softmax'))
+
+    t0 = time.clock()
+    run_model(model1, 1, X_train, X_val, X_test, y_train, y_val, y_test)
+    t1 = time.clock()
+    TIME1 = t1 - t0
+    run_model(model2, 2, X_train, X_val, X_test, y_train, y_val, y_test)
+    t2 = time.clock()
+    TIME2 = t2 - t1
+    run_model(model3, 3, X_train, X_val, X_test, y_train, y_val, y_test)
+    t3 = time.clock()
+    TIME3 = t3 - t2
+    times = open("times.txt", 'w+')
+    times.write("Model 1: ")
+    times.write(TIME1)
+    times.write("Model 2: ")
+    times.write(TIME2)
+    times.write("Model 3: ")
+    times.write(TIME3)
+
+
+def run_model(model, index, X_train, X_val, X_test, y_train, y_val, y_test):
+    file = open("model" + str(index) + ".txt", 'w+')
+    # Model summary
+    file.write("MODEL NR " + str(index))
+    model.summary()
+    file.write("\n")
+
+    # Compile the model
+    model.compile(optimizer='adam', metrics=['accuracy'], loss='categorical_crossentropy')
+
+    # Use ModelCheckpoint to save the best model from all epochs
+    callback = [ModelCheckpoint(filepath='best_model' + str(index) + '.h5', monitor='val_accuracy', save_best_only=True,
+                                mode='max')]
+
+    # Fit the model with train and validation data
+    model.fit(X_train, y_train, validation_data=(X_val, y_val), batch_size=BATCH_SIZE, epochs=EPOCHS, verbose=1,
+              callbacks=callback)
+
+    # Plot model accuracy through all epochs
+    accuracy_plot(model, index, file)
+    show_mistakes(model, index)
+
+    # Load in the best model from ModelCheckpoint
+    best_model = load_model('best_model.h5')
+
+    # Check the best model accuracy on test data
+    file.write("\nWYNIKI TESTÓW: ")
+    file.write(best_model.evaluate(X_test, y_test))
+    file.write("---------------------------------------------------------\n")
+    file.close()
+
+
+def accuracy_plot(model, index, file):
+    best_accuracy = max(model.history.history['val_accuracy'])
+    best_epoch = model.history.history['val_accuracy'].index(best_accuracy)
+    print('\n----------------- Wyuczony najlepszy model --------------------')
+    print('--------- Epoka: {}, najlepsze dopasowanie: {} -----------------').format(best_epoch, best_accuracy)
+    file.write('\n----------------- Wyuczony najlepszy model --------------------')
+    file.write('--------- Epoka: ' + str(best_epoch) + ', najlepsze dopasowanie: ' + str(best_accuracy) + ' -----------------')
+    plt.plot(model.history.history['val_accuracy'])
+    plt.title('Test Accuracy by Epoch')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.ylim(0.8, 1)
+    plt.draw()
+    plt.savefig('model' + str(index) + '.png')
+
+
+def show_mistakes(model, index, X_test, y_test):
+    classes = ['T-shirt/Top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag',
+               'Ankle Boot']
+
+    # Create Multiclass Confusion Matrix
+
+    preds = model.predict(X_test)
+    cm = confusion_matrix(np.argmax(y_test, axis=1), np.argmax(preds, axis=1))
+
+    plt.figure(figsize=(8, 8))
+    plt.imshow(cm, cmap=plt.cm.Blues)
+    plt.title('Fashion MNIST Confusion Matrix - CNN')
+    plt.colorbar()
+    plt.xticks(np.arange(10), classes, rotation=90)
+    plt.yticks(np.arange(10), classes)
+
+    for i, j in product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > 500 else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.savefig('model' + str(index) + '_mistakes.png')
---
+++ main.py
@@ -0,0 +1,78 @@
+import matplotlib.pyplot as plt
+from keras.utils import np_utils
+from keras.datasets import fashion_mnist
+import numpy as np
+
+from knn import score_knn
+from cnn import run_cnn
+
+
+def classification_KNN_vs_no_neighbours(xs, ys):
+    plt.rcParams['image.cmap'] = 'gray'
+    plt.rcParams['image.interpolation'] = 'none'
+    # plt.style.use(['dark_background'])
+    plt.xlabel('Liczba sasiadow k')
+    plt.ylabel('Blad klasyfikacji')
+    plt.title("Selekcja modelu dla k-NN")
+    plt.plot(xs, ys, 'r-', color='#FFCC55')
+    plt.draw()
+    # plt.waitforbuttonpress(0)
+
+
+def loadData():
+    (X_train, y_train), (X_test, y_test) = fashion_mnist.load_data()
+    # X_test, y_test = fashion_mnist.load_data('data/fashion', kind='t10k')
+    # Extracting 6000 images for validation data
+    X_val = X_train[54000:60000]
+    X_train = X_train[0:54000]
+    y_val = y_train[54000:60000]
+    y_train = y_train[0:54000]
+    return X_train, y_train, X_val, y_val, X_test, y_test
+
+
+def run_knn(X_train, y_train, X_val, y_val, X_test, y_test):
+    # KNN model selection
+    k_values = range(1, 101, 2)
+    print('\n--- Selekcja liczby sasiadow dla modelu dla KNN i benchmark na danych testowych ---')
+    print('-------------------- Wartosci k: 1, 3, ..., 100 -----------------------')
+    score, error_best, best_k, errors = score_knn(X_train, X_val, X_test, y_train, y_val, y_test, k_values)
+    print('Najlepsze k: {num1} i najlepszy blad: {num2:.4f}'.format(num1=best_k, num2=error_best))
+    print('DOPASOWANIE MODELU KNN NA DANYCH TESTOWYCH: {num}'.format(num=score))
+    print('\n-----------------------------------------------------------------------\n')
+    print('\n--- Wcisnij klawisz, aby kontynuowac ---')
+    classification_KNN_vs_no_neighbours(k_values, errors)
+
+
+def preprocess_data(X_train, y_train, X_val, y_val, X_test, y_test):
+    # Reshape the data to 28x28 pixels
+    X_train = X_train.reshape(X_train.shape[0], 28, 28, 1)
+    X_val = X_val.reshape(X_val.shape[0], 28, 28, 1)
+    X_test = X_test.reshape(X_test.shape[0], 28, 28, 1)
+    # Scale values to a range from 0 to 1 and convert them to float
+    X_train = X_train.astype("float32") / 255.
+    X_val = X_val.astype("float32") / 255.
+    X_test = X_test.astype("float32") / 255.
+    # Change 0-9 labels values to one-hot encoding, for example 2 => [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+    y_train = np_utils.to_categorical(y_train, 10)
+    y_val = np_utils.to_categorical(y_val, 10)
+    y_test = np_utils.to_categorical(y_test, 10)
+    return X_train, y_train, X_val, y_val, X_test, y_test
+
+
+def run_models():
+    # Loading fashion mnist data
+    X_train, y_train, X_val, y_val, X_test, y_test = loadData()
+
+    # Run KNN
+    # run_knn(X_train, y_train, X_val, y_val, X_test, y_test)
+
+    # Neural network model testing
+    # Preprocess data before modeling
+    X_train, y_train, X_val, y_val, X_test, y_test = preprocess_data(X_train, y_train, X_val, y_val, X_test, y_test)
+
+    # run CNN
+    run_cnn(X_train, X_val, X_test, y_train, y_val, y_test)
+
+
+if __name__ == '__main__':
+    run_models()
